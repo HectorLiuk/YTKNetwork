b6fb00ba911f7d4769936829caf615f5ef56671b
snail_lk <544523660@qq.com>
2016-11-25 15:37:57 +0800

    add

---
+++ YTKNetworkDemo/YTKNetworkDemo/LearnApi.h
@@ -0,0 +1,13 @@
+//
+//  LearnApi.h
+//  YTKNetworkDemo
+//
+//  Created by Hector on 2016/11/25.
+//  Copyright © 2016年 yuantiku.com. All rights reserved.
+//
+
+#import <YTKNetwork/YTKNetwork.h>
+
+@interface LearnApi : YTKRequest
+
+@end
---
+++ YTKNetworkDemo/YTKNetworkDemo/LearnApi.m
@@ -0,0 +1,13 @@
+//
+//  LearnApi.m
+//  YTKNetworkDemo
+//
+//  Created by Hector on 2016/11/25.
+//  Copyright © 2016年 yuantiku.com. All rights reserved.
+//
+
+#import "LearnApi.h"
+
+@implementation LearnApi
+
+@end
